ab212c6d020a84a6dac92db9ebeeab91a6f5b58d
Joao Lemos <jhflemos@gmail.com>
2025-11-04 16:46:11 +0100

    Format Tf files

--- aws/alb.tf
+++ aws/alb.tf
@@ -7,29 +7,29 @@ resource "aws_lb" "app_alb" {
 }
 
 resource "aws_lb_target_group" "ecs_tg" {
- name     = "${var.environment}-ecs-tg"
- port     = 80
- protocol = "HTTP"
- vpc_id   = module.vpc.vpc_id
+  name     = "${var.environment}-ecs-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
 
- health_check {
-   path = "/"
-   interval = 30
-   timeout = 5
-   healthy_threshold = 2
-   unhealthy_threshold = 2
-   matcher = "200"
- }
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
 resource "aws_lb_listener" "http_listener" {
- load_balancer_arn = aws_lb.app_alb.arn
- port              = 80
- protocol          = "HTTP"
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
- default_action {
-   type = "forward"
-   target_group_arn = aws_lb_target_group.ecs_tg.arn
- }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+  }
 }
 
