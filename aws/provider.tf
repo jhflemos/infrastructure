222bf0e79b9e7003ebe2fb6bb465a3ff0821dfde
Joao Lemos <jhflemos@gmail.com>
2025-11-04 16:53:30 +0100

    Fixing VPC module reference

--- aws/provider.tf
+++ aws/provider.tf
@@ -9,10 +9,4 @@ provider "aws" {
 }
 terraform {
   required_version = ">= 1.5.0"
-
-  backend "s3" {
-    bucket = "study-terraform-state-bucket"
-    key    = "aws/env/${var.environment}/terraform.tfstate"
-    region = var.region
-  }
 }
